37b6313fa42f2c4eb4664008c7dbfc5b69595a84
mozsnow <67405205+mozsnow@users.noreply.github.com>
2020-10-13 22:46:31 -0400

    min_max

--- min_max.py
+++ min_max.py
@@ -1,8 +1,10 @@
-
+# get input from user
 num_1 = int(input("How many integers would you like to enter?"))
-print("Please enter", num_1, "integers.")
+# enter -4, 105,2, -7
+print("Please enter", num_1, "integers:")
 _min = int(input())
 _max = _min
+# integers are to be arranged from min to max
 for i in range(1, num_1):
     number = int(input())
     if number > _max:
